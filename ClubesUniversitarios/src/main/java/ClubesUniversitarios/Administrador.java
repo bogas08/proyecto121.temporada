32da8d03800f6f34048661f5ebc990c267f7ea3a
bogas08 <lopezgabriel0802>
2025-07-23 00:37:16 -0400

    clase Administrador

--- ClubesUniversitarios/src/main/java/ClubesUniversitarios/Administrador.java
+++ ClubesUniversitarios/src/main/java/ClubesUniversitarios/Administrador.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ClubesUniversitarios;
-
-/**
- *
- * @author HP
- */
-public class Administrador {
-    
+public class Administrador extends Usuario {
+    private String permisos;
+ 
+    public Administrador(String id, String nom, String cor, String per) {
+        super(id, nom, cor);
+        this.permisos = per;
+    }
+ 
+    public void crearActividad() {
+        System.out.println("Actividad creada.");
+    }
+ 
+    public void eliminarActividad() {
+        System.out.println("Actividad eliminada.");
+    }
+ 
+    public void registrar() {
+        System.out.println("Administrador registrado: " + nombre);
+    }
+ 
+    public void mostrar() {
+        System.out.println("Administrador: " + nombre + ", Correo: " + correo + ", Permisos: " + permisos);
+    }
+ 
+    // Getters y setters
+    public String getPermisos() { return permisos; }
+    public void setPermisos(String permisos) { this.permisos = permisos; }
 }
